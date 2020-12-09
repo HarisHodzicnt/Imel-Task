a9f3fbcc772e3d6fe7bc3e7570177dc5734a8a98
HarisHodzicnt <haris_hodzicnt@hotmail.com>
2020-12-09 18:26:56 +0100

    final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg"
+import "./App.css"
+import HomePage from "./HomePage"
+import Category from "./Category"
+import NavBar from "./NavBar"
+import Footer from "./Footer"
+import Checkout from "./checkout/CheckOut"
+import {
+  BrowserRouter as Router,
+  NavLink,
+  Route,
+  Switch,
+} from "react-router-dom"
+import ProductPage from "./ProductPage"
+import NoRoute from "./NoRoute"
+
+import { useEffect, useState } from "react"
 
 function App() {
+  useEffect(() => {
+    const cart = JSON.parse(localStorage.getItem("cart"))
+    var quantity = cart ? parseInt(cart?.map((item) => item.quantity).reduce((a, b) => a + b, 0)) : 0
+    setCartQuantity(quantity)
+  }, [])
+
+  const [cartQuantity, setCartQuantity] = useState()
+  const [showMenu, setShowMenu] = useState()
+
+  const updateCartQuantity = () => {
+    setCartQuantity(cartQuantity + 1)
+    setShowMenu(true)
+  }
+
+  const handleShowMenu = (value) => {
+    setShowMenu(value)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div className="App">
+        <header>
+          {window.location.pathname !== "/checkout" ? (
+            <NavBar
+              cartQuantity={cartQuantity}
+              showMenu={showMenu}
+              handleShowMenu={handleShowMenu}
+            />
+          ) : (
+            false
+          )}
+        </header>
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+          <Route exact path="/women">
+            <Category />
+          </Route>
+          <Route exact path="/men">
+            <Category />
+          </Route>
+          <Route exact path="/men/:id">
+            <ProductPage updateCartQuantity={updateCartQuantity} />
+          </Route>
+          <Route exact path="/women/:id">
+            <ProductPage updateCartQuantity={updateCartQuantity} />
+          </Route>
+          <Route exact path="/checkout">
+            <Checkout />
+          </Route>
+          <Route>
+            <NoRoute />
+          </Route>
+        </Switch>
+      </div>
+      <footer>
+        <Footer />
+      </footer>
+    </Router>
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './fonts/Inter-VariableFont_slnt,wght.ttf';
+
+
 
 ReactDOM.render(
   <React.StrictMode>
